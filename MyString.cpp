873b39dc23ca870545d27284ad00bf5f44f331cd
Andrey <crashc@yandex.ru>
2019-04-14 16:45:14 +0500

    Create MyString.cpp

---
+++ MyString.cpp
@@ -0,0 +1,20 @@
+MyString & MyString::replace(const MyString & fromFormatStr,
+                             const MyString & toFormatStr,
+                             const int numberEntries)
+{
+  if (0 > numberEntries)
+    return *this;
+    
+  const char * fountStr = strstr(_str, fromFormatStr._str);
+  
+  if (!foundStr)
+    return *this;
+    
+  if ((toFormatStr.size() > _size) ||
+      (formFormatStr.size() < toFormatStr._size))
+  {
+    cout << "Exit!" << endl;
+    return *this;
+  }
+      
+}
